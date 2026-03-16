87737b7e895f0118b209aa951ee5d8d55206533f
Doug Wegscheid <dwegscheid@sbcglobal.net>
2026-03-16 11:58:05 -0400

    Turn off heater when disabled.

--- src/main/java/frc/robot/FancyTestCommand.java
+++ src/main/java/frc/robot/FancyTestCommand.java
@@ -34,6 +34,7 @@ public class FancyTestCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    heaterSubsystem.setHeaterPower(0);
     fsm.setState(null);
   }
 
